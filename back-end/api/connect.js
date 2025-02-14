cbe4d73d064fae4bfb81472b03f1d3b5beb19d69
Mauricio <mauricio_cy@hotmail.com>
2025-02-14 14:19:42 -0300

    adding db_pass

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,8 +1,14 @@
+import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 
-const URI =
-  "mongodb+srv://admin:fe0vGpGgKbbZeVaP@spotify-project.dysgh.mongodb.net/?retryWrites=true&w=majority&appName=spotify-project";
+dotenv.config();
 
-const client = new MongoClient(URI);
+const DB_PASS = process.env.DB_PASS;
+const URL =
+  `mongodb+srv://admin:${DB_PASS}@spotify-project.dysgh.mongodb.net/?retryWrites=true&w=majority&appName=spotify-project"`;
+
+console.log(URL)
+
+const client = new MongoClient(URL);
 
 export const db = client.db("db-spotify");
