aaa79b1d9e15a061ef5e07fe0582aedae45e392b
Mauricio <mauricio_cy@hotmail.com>
2025-02-14 13:13:23 -0300

    deploy adjustments

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,11 @@
+import "dotenv/config";
 import axios from "axios";
 
-const url = "http://localhost:3001";
+const { NODE_ENV } = process.env;
+const url = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
 
 const resArtists = await axios.get(`${url}/artists`);
 const resSongs = await axios.get(`${url}/songs`);
 
 export const artistArray = resArtists.data;
-export const songsArray = resSongs.data
+export const songsArray = resSongs.data;
