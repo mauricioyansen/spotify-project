80703e103c290718bbce0170e9bac482db3760c2
Mauricio <mauricio_cy@hotmail.com>
2025-02-14 13:44:24 -0300

    Fix process.env issue in Vite config

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {}, // Evita erro "process is not defined" no navegador
+  },
   build: {
     target: "esnext",
   }
